574b09d50aada738ac9ccfbb1f022130d27aa130
abhisheks28 <abhishekmys03@gmail.com>
2025-12-18 19:04:51 +0530

    popup img updated (#6)
    * deploy yml changed
    
    * updated yml
    
    * popup photos update

--- src/components/LifeAtLGSClient.tsx
+++ src/components/LifeAtLGSClient.tsx
@@ -60,7 +60,7 @@ const LifeAtLGSClient: React.FC<LifeAtLGSClientProps> = ({ initialGalleryItems }
             achievement: "Board Exam Topper",
             description: "Secured top rank in the district",
             year: "2024",
-            image: "/popup/2.jpg",
+            image: "/popup/02.png",
             details: "Achieved highest marks in the annual examinations."
         },
         {
@@ -70,7 +70,7 @@ const LifeAtLGSClient: React.FC<LifeAtLGSClientProps> = ({ initialGalleryItems }
             achievement: "Distinction Holder",
             description: "Consistent academic excellence",
             year: "2024",
-            image: "/popup/3.jpeg",
+            image: "/popup/03.png",
             details: "Maintained distinction throughout the academic year."
         },
         {
@@ -80,7 +80,7 @@ const LifeAtLGSClient: React.FC<LifeAtLGSClientProps> = ({ initialGalleryItems }
             achievement: "Subject Excellence",
             description: "Highest marks in core subjects",
             year: "2024",
-            image: "/popup/4.jpg",
+            image: "/popup/04.png",
             details: "Expertise and in-depth knowledge in core subjects."
         },
 
